a729daa1113700223b9dabf1674226b6c39870fb
Noah Foilb <nofoilb14@gmail.com>
2024-02-23 11:34:44 -0500

    Inclass activities

--- src/quiz/basic-server.js
+++ src/quiz/basic-server.js
@@ -45,7 +45,11 @@ const server = http.createServer((req, res) => {
             res.end(JSON.stringify(incidents.filter((incident) => {
                 return incident.id === `MABOS00${id}`
             })));
+        } else {
+           res.writeHead(400, { 'Content-Type': 'text/plain' });
+            res.end('Missing parameters in the URL'); 
         }
+
     } else {
         // If the request is for an unsupported endpoint, send a not found response
         res.writeHead(404, { 'Content-Type': 'text/plain' });
